e09958841e0d723b33a203a25c6e3fd76926b830
Jayden Wise <jayden.wise@hotmail.com>
2018-02-17 14:54:32 +0100

    added s41 line

--- js/index.js
+++ js/index.js
@@ -14,23 +14,23 @@ $(document).on('mousemove', function(e) {
 //
 
 //
-//
-// var pathEls = document.querySelectorAll('path');
-// // var pathEls = document.getElementsByClassName('path1');
-//
-//
-// for (var i = 0; i < pathEls.length; i++) {
-//   var pathEl = pathEls[i];
-//   var offset = anime.setDashoffset(pathEl);
-//   pathEl.setAttribute('stroke-dashoffset', offset);
-//   anime({
-//     targets: pathEl,
-//     strokeDashoffset: [offset, 0],
-//     duration: anime.random(1000, 3000),
-//     delay: anime.random(0, 2000),
-//     loop: true,
-//     direction: 'alternate',
-//     easing: 'easeInOutSine',
-//     autoplay: true
-//   });
-// }
+
+var pathEls = document.querySelectorAll('path');
+// var pathEls = document.getElementsByClassName('path1');
+
+
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute('stroke-dashoffset', offset);
+  anime({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: anime.random(1000, 3000),
+    delay: anime.random(0, 2000),
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
+}
